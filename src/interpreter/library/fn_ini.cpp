9d245404b7aa226cf6e76b8ea6266cf1ce369376
Grossley <42724744+Grossley@users.noreply.github.com>
2021-09-12 12:37:47 -0700

    Try fix ini_read_real

--- src/interpreter/library/fn_ini.cpp
+++ src/interpreter/library/fn_ini.cpp
@@ -111,8 +111,16 @@ void ogm::interpreter::fn::ini_read_real(VO out, V section, V key, V d)
         _sec.c_str(), _key.c_str(), std::to_string(_def).c_str()
     );
 
-    real_t r = std::stod(value);
-    out = r;
+    try
+    {
+        real_t r = std::stod(value);
+        out = r;
+    }
+    catch (std::exception& e)
+    {
+        // backup in case no string was found.
+        out = 0;
+    }
 }
 
 void ogm::interpreter::fn::ini_section_exists(VO out, V section)
